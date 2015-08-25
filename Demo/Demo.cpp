f55bdf3d64c5ea6452e860ad5c37d76d5e924882
Scott Kolodziej <spkolodziej@gmail.com>
2015-08-24 20:46:34 -0500

    Remove meshpart

--- Demo/Demo.cpp
+++ Demo/Demo.cpp
@@ -7,32 +7,37 @@ using namespace SuiteSparse_Mongoose;
 int main(int argn, const char **argv)
 {
     /* Get the input file from the console input. */
-    const char *inputFile = NULL;
-    if(argn == 2) { inputFile = argv[1]; }
-    else { printf("usage: Mongoose MM-input-file\n"); return 0; }
+    const char *inputFile = NULL ;
+    if (argn == 2)
+        inputFile = argv[1] ;
+    else
+    {
+        printf ("Usage: Demo <MM-input-file.mtx>\n") ;
+        return 0 ;
+    }
 
-    Options *O = Options::Create();
-    if(!O) return 1; // Return an error if we failed.
+    Options *options = Options::Create();
+    if (!options) return 1 ; // Return an error if we failed.
 
-    O->doExpensiveChecks = true;
-    O->matchingStrategy = HEMDavisPA;
-    O->guessCutType = QP_BallOpt;
+    options->doExpensiveChecks = true ;
+    options->matchingStrategy = HEMDavisPA ;
+    options->guessCutType = QP_BallOpt ;
 
     /* Read & condition the matrix market input. */
-    Graph *U = conditionGraph(readGraphFromMM(inputFile), O);
+    Graph *U = conditionGraph (readGraphFromMM (inputFile), options) ;
 
-    ComputeEdgeSeparator(U, O);
+    ComputeEdgeSeparator (U, options) ;
 
-printf("U->cutCost = %f\n", U->cutCost);
-printf("U->imbalance = %f\n", U->imbalance);
-printf("Partitioning Complete\n");
+    printf ("U->cutCost = %f\n", U->cutCost) ;
+    printf ("U->imbalance = %f\n", U->imbalance) ;
+    printf ("Partitioning Complete\n") ;
 
-    U->~Graph();
-    free(U);
+    U->~Graph () ;
+    free (U) ;
 
-    O->~Options();
-    free(O);
+    options->~Options () ;
+    free (options) ;
 
     /* Return success */
-    return 0;
+    return 0 ;
 }
