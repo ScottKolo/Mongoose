aa8496fd853c3b95ef91309f50248fef9371d54b
Scott Kolodziej <spkolodziej@gmail.com>
2016-06-14 21:59:17 -0500

    Simplify Mongoose executable

--- Executable/Mongoose.cpp
+++ Executable/Mongoose.cpp
@@ -2,17 +2,18 @@
 #include "mongoose.hpp"
 #include "mongoose_conditioning.hpp"
 #include "mongoose_cs.hpp"
+#include "mongoose_io.hpp"
 #include <ctime>
 
 using namespace Mongoose;
 
 int main(int argn, const char **argv)
 {
-    std::clock_t start;
+    // Start the overall time clock
+    std::clock_t start = std::clock();
     double duration;
 
-    start = std::clock();
-    /* Get the input file from the console input. */
+    // Get the input file from the console input.
     const char *inputFile = NULL ;
     if (argn == 2)
         inputFile = argv[1] ;
@@ -29,49 +30,26 @@ int main(int argn, const char **argv)
     options->matchingStrategy = HEMDavisPA ;
     options->guessCutType = QP_BallOpt ;
 
-    /* Read & condition the matrix market input. */
-    Graph *U = conditionGraph (readGraphFromMM (inputFile), options) ;
-    /* Only process the largest connected component */
-    cs *B = GraphToCSparse3(U, 1);
-    csd* dmperm = cs_scc (B) ;
-    int largest_size = 0 ;
-    int largest_scc = 0 ;
-    int scc_size = 0 ;
-    for (int i = 0 ; i < dmperm->nb ; i++)
+    Graph *U = read_graph(inputFile);
+    if (!U)
     {
-        scc_size = dmperm->r[i+1]-1 - dmperm->r[i] ;
-        if (scc_size > largest_size)
-        {
-            largest_size = scc_size ;
-            largest_scc = i ;
-        }
+        free(options);
+        return 1;
     }
 
-    // Get submatrix from dmperm
-    csi *pinv = cs_pinv(dmperm->p, B->n);
-    cs *C = cs_permute(B, pinv, dmperm->p, 1);
-    cs *submatrix = cs_submat(C, dmperm->r[largest_scc], 
-                                dmperm->r[largest_scc+1]-1, 
-                                dmperm->r[largest_scc], 
-                                dmperm->r[largest_scc+1]-1) ;
-    Graph *V = CSparse3ToGraph(submatrix);
-    U = conditionGraph (V, options) ;
     ComputeEdgeSeparator (U, options) ;
 
-    printf ("U->cutCost = %f\n", U->cutCost) ;
-    printf ("U->imbalance = %f\n", U->imbalance) ;
-    printf ("Partitioning Complete\n") ;
+    printf ("Partitioning Complete!\n") ;
+    printf ("Cut Cost:      %.2f\n", U->cutCost) ;
+    printf ("Cut Imbalance: %.2f%%\n", 100*U->imbalance) ;
+    
+    U->~Graph ();
+    free(U);
+    free(options);
 
-    U->~Graph () ;
-    free (U) ;
+    duration = (std::clock() - start) / (double) CLOCKS_PER_SEC;
+    printf("Total Time:    %.2fs\n", duration);
 
-    options->~Options () ;
-    free (options) ;
-
-    duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
-
-    printf("Total Time: %f\n", duration);
-
-    /* Return success */
+    // Return success
     return 0 ;
 }
