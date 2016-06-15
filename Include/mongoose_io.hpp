68f32d1602fcf066954a9c289ac04e31b202e84a
Scott Kolodziej <spkolodziej@gmail.com>
2016-06-14 21:58:47 -0500

    Simplified I/O

--- Include/mongoose_io.hpp
+++ Include/mongoose_io.hpp
@@ -3,13 +3,18 @@
 
 #include "mongoose_cs.hpp"
 #include "mongoose_internal.hpp"
+#include <string>
 
 namespace Mongoose
 {
 
+Graph *read_graph (const std::string filename);
+cs *read_matrix (const std::string filename);
 Graph *read_graph (const char* filename);
 cs *read_matrix (const char* filename);
 void get_triangular(cs *A, bool lower);
+void remove_diagonal(cs *A);
+cs *mirror_triangular(cs *A);
 
 } // end namespace Mongoose
 
--- Source/mongoose_io.cpp
+++ Source/mongoose_io.cpp
@@ -1,6 +1,7 @@
 
 #include "mongoose_io.hpp"
 #include "mongoose_interop.hpp"
+#include "mongoose_debug.hpp"
 #include <cstdio>
 #include <iostream>
 extern "C" {
@@ -12,9 +13,20 @@ using namespace std;
 namespace Mongoose
 {
 
+Graph *read_graph (const std::string filename)
+{
+    return read_graph(filename.c_str());
+}
+
+cs *read_matrix (const std::string filename)
+{
+    return read_matrix(filename.c_str());
+}
+
 Graph *read_graph (const char* filename)
 {
     cs* A = read_matrix(filename);
+    if (!A) return NULL;
     Graph *G = CSparse3ToGraph(A);
     return G;
 }
@@ -35,9 +47,12 @@ cs *read_matrix (const char* filename)
         return NULL;
     }
     if (!mm_is_matrix(matcode) || !mm_is_sparse(matcode) || 
-        mm_is_complex(matcode))
+        mm_is_complex(matcode) || !mm_is_symmetric(matcode))
     {
-        cout << "Unsupported matrix format - Must be real and sparse." << endl;
+        cout << 
+            "Unsupported matrix format - Must be real, sparse, and symmetric"
+             << endl;
+        return NULL;
     }
 
     Int M, N, nz;
@@ -57,6 +72,7 @@ cs *read_matrix (const char* filename)
     {
         --I[k];
         --J[k];
+        if (mm_is_pattern(matcode)) val[k] = 1;
     }
 
     cs *A = (cs *) SuiteSparse_malloc(1, sizeof(cs));
@@ -69,9 +85,31 @@ cs *read_matrix (const char* filename)
     A->nz = nz;
 
     cs* compressed_A = cs_compress(A);
-    get_triangular(compressed_A, true);
+    remove_diagonal(compressed_A);
+    compressed_A = mirror_triangular(compressed_A);
+    csd* dmperm = cs_scc (compressed_A) ;
+    int largest_size = 0 ;
+    int largest_scc = 0 ;
+    int scc_size = 0 ;
+    for (int i = 0 ; i < dmperm->nb ; i++)
+    {
+        scc_size = dmperm->r[i+1]-1 - dmperm->r[i] ;
+        if (scc_size > largest_size)
+        {
+            largest_size = scc_size ;
+            largest_scc = i ;
+        }
+    }
 
-    return compressed_A;
+    // Get submatrix from dmperm
+    csi *pinv = cs_pinv(dmperm->p, compressed_A->n);
+    cs *C = cs_permute(compressed_A, pinv, dmperm->p, 1);
+    cs *submatrix = cs_submat(C, dmperm->r[largest_scc], 
+                                dmperm->r[largest_scc+1]-1, 
+                                dmperm->r[largest_scc], 
+                                dmperm->r[largest_scc+1]-1) ;
+
+    return submatrix;
 }
 
 void get_triangular(cs *A, bool lower)
@@ -95,6 +133,60 @@ void get_triangular(cs *A, bool lower)
         old_Ap = Ap[j+1];
         Ap[j+1] = nz;
     }
+}
+
+void remove_diagonal(cs *A)
+{
+    Int n = A->n;
+    Int *Ap = A->p; Int *Ai = A->i; double *Ax = A->x;
+    Int nz = 0;
+    Int old_Ap = Ap[0];
+
+    for (Int j = 0; j < n; j++)
+    {
+        for (Int p = old_Ap; p < Ap[j+1]; p++)
+        {
+            if (Ai[p] != j)
+            {
+                Ai[nz] = Ai[p];
+                Ax[nz] = Ax[p];
+                nz++;
+            }
+        }
+        old_Ap = Ap[j+1];
+        Ap[j+1] = nz;
+    }
+}
+
+// Requires A to be a triangular matrix with no diagonal.
+cs *mirror_triangular(cs *A)
+{
+    Int A_n = A->n; Int A_nz = A->p[A_n];
+    Int B_nz = 2*A_nz;
+
+    cs *B = cs_spalloc (A_n, A_n, B_nz, 1, 1);
+
+    Int *Ap = A->p; Int *Ai = A->i; double *Ax = A->x;
+    Int *Bp = B->p; Int *Bi = B->i; double *Bx = B->x;
+    Int nz = 0;
+
+    for (Int j = 0; j < A_n; j++)
+    {
+        for (Int p = Ap[j]; p < Ap[j+1]; p++)
+        {
+            Bi[nz] = Ai[p];
+            Bp[nz] = j;
+            Bx[nz] = Ax[p];
+            nz++;
+            Bi[nz] = j;
+            Bp[nz] = Ai[p];
+            Bx[nz] = Ax[p];
+            nz++;
+        }
+    }
+    B->nz = nz;
+
+    return cs_compress(B);
 }
 
 } // end namespace Mongoose
