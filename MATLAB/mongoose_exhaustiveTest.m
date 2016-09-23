9b8828c8cbb446660208b8165c0cc97eed2e0a44
Scott Kolodziej <spkolodziej@gmail.com>
2016-09-22 19:42:36 -0500

    Add (mostly) exhaustive test in MATLAB API

---
+++ MATLAB/mongoose_exhaustiveTest.m
@@ -0,0 +1,21 @@
+function mongoose_exhaustiveTest
+    index = UFget;
+
+    for i = 1:length(index.nrows)
+        if (index.isReal(i) && index.numerical_symmetry(i) && index.nnz(i) < 1E6)
+            Prob = UFget(i);
+            fprintf('Computing separator for %d: %s\n', i, Prob.name);
+            A = Prob.A;
+
+            % Sanitize the matrix: remove diagonal elements, check for positive edge
+            % weights, and make sure it is symmetric.
+            A = mongoose_sanitizeMatrix(A);
+
+            % Run Mongoose to partition the graph.
+            tic
+            partition = mongoose_computeEdgeSeparator(A);
+            toc
+            fprintf('\n');
+        end
+    end
+end
