b623f5c58add4d46c1ffc2704ab200d202dd8322
Scott Kolodziej <spkolodziej@gmail.com>
2016-10-30 18:33:12 -0500

    Add simple Python script for running Mongoose and METIS for comparison

--- Executable/Mongoose.cpp
+++ Executable/Mongoose.cpp
@@ -9,50 +9,75 @@ using namespace Mongoose;
 
 int main(int argn, const char **argv)
 {
-    Logger log(Info, true);
+    SuiteSparse_start();
 
-    // Start the overall time clock
-    std::clock_t start = std::clock();
-    double duration;
+    clock_t t;
+    
+    // Set Logger to report only Error messages
+    Logger::setDebugLevel(Error);
 
-    // Get the input file from the console input.
-    const char *inputFile = NULL ;
-    if (argn == 2)
-        inputFile = argv[1] ;
-    else
+    if (argn != 2)
     {
-        printf ("Usage: Demo <MM-input-file.mtx>\n") ;
-        return 0 ;
-    }
-
-    Options *options = Options::Create();
-    if (!options) return 1 ; // Return an error if we failed.
-
-    options->doExpensiveChecks = false ;
-    options->matchingStrategy = HEMDavisPA ;
-    options->guessCutType = QP_BallOpt ;
-
-    Graph *U = readGraph(inputFile);
-    if (!U)
-    {
-        free(options);
+        // Wrong number of arguments - return error
+        Logger::log(Error, "Usage: Demo <MM-input-file.mtx>");
+        SuiteSparse_finish();
         return 1;
     }
 
-    ComputeEdgeSeparator(U, options) ;
-
-    printf ("Partitioning Complete!\n") ;
-    printf ("Cut Cost:      %.2f\n", U->cutCost) ;
-    printf ("Cut Imbalance: %.2f%%\n", 100*U->imbalance) ;
+    // Read in input file name
+    std::string inputFile = std::string(argv[1]);
     
-    U->~Graph ();
-    free(U);
-    free(options);
+    // Turn timing information on
+    Logger::setTimingFlag(true);
 
-    duration = (std::clock() - start) / (double) CLOCKS_PER_SEC;
-    printf("Total Time:    %.2fs\n", duration);
-    Logger::printTimingInfo();
+    Options *options = Options::Create();
+    if (!options)
+    {
+        // Ran out of memory
+        Logger::log(Error, "Error creating Options struct");
+        SuiteSparse_free(options);
+        return 1;
+    }
 
-    // Return success
+    Graph *G = readGraph(inputFile);
+
+    if (!G)
+    {
+        // Ran out of memory or problem reading the graph from file
+        Logger::log(Error, "Error reading Graph from file");
+        SuiteSparse_free(options);
+        SuiteSparse_free(G);
+        return 1;
+    }
+
+    // An edge separator should be computed with default options
+    t = clock();
+    int error = ComputeEdgeSeparator(G, options);
+    t = clock() - t;
+
+    if (error)
+    {
+        // Error occurred
+        Logger::log(Error, "Error computing edge separator in Performance Test");
+        SuiteSparse_free(options);
+        SuiteSparse_free(G);
+        return 1;
+    }
+    else
+    {
+        double test_time = ((double) t)/CLOCKS_PER_SEC;
+        Logger::log(All, "Total Edge Separator Time: " + std::to_string(test_time) + "s");
+        Logger::printTimingInfo();
+        Logger::log(All, "Cut Properties:");
+        Logger::log(All, " Cut Cost:  " + std::to_string(G->cutCost));
+        Logger::log(All, " Imbalance: " + std::to_string(G->imbalance));
+    }
+
+    G->~Graph();
+    SuiteSparse_free(G);
+    SuiteSparse_free(options);
+
+    SuiteSparse_finish();
+
     return 0 ;
 }
---
+++ Tests/runCompare.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+from __future__ import absolute_import, print_function, unicode_literals
+from subprocess import call, check_output
+import os       # For filesystem access
+import sys      # For sys.exit()
+import argparse # For parsing command-line arguments
+import urllib   # For downloading the UFget index
+import tarfile  # For un-tar/unzipping matrix files
+import csv      # For reading the UFget index
+import shutil   # For using 'which'
+from distutils.spawn import find_executable # For using find_executable
+
+parser = argparse.ArgumentParser(
+                    description='Run tests on the Mongoose library.')
+parser.add_argument('-k', '--keep', 
+                    action='store_true', 
+                    help='do not remove downloaded files when test is complete')
+parser.add_argument('-p', '--purge', 
+                    action='store_true', 
+                    help='force remove downloaded matrix files when complete')
+parser.add_argument('-min', 
+                    action='store', 
+                    metavar='max_id', 
+                    type=int, 
+                    default=1,
+                    help='minimum matrix ID to run tests on [default: 1]')
+parser.add_argument('-max', 
+                    action='store', 
+                    metavar='min_id', 
+                    type=int, 
+                    default=2757,
+                    help='maximum matrix ID to run tests on [default: 2757]')
+parser.add_argument('-i', '--ids', 
+                    action='store', 
+                    nargs='+', 
+                    metavar='matrix_ID', 
+                    type=int,
+                    help='list of matrix IDs to run tests on')
+parser.add_argument('-d', '--matrix-directory', 
+                    action='store', 
+                    metavar='matrix_dir',
+                    default='../Matrix',
+                    help='download directory for Matrix Market data files')
+
+args = parser.parse_args()
+
+# Check if we are in the right location - if not, exit
+if not (os.path.isdir('./CMakeFiles/mongoose_dbg.dir/Source')
+        and os.path.isdir('../Source')):
+    print(
+        "\n\033[91mERROR!\033[0m Looks like you might not be running this from "
+        "your build directory.\n\n"
+        "Make sure that... \n\n"
+        "  * You are in your build directory (e.g. Mongoose/_build) and\n"
+        "  * You have built Mongoose ('cmake ..' followed by 'make')\n")
+    sys.exit()
+
+# Check if Mongoose has been built - if not, exit
+if not (os.path.exists('./CMakeFiles/mongoose_dbg.dir/Source/Mongoose_Graph.o')):
+    print(
+        "\n\033[91mERROR!\033[0m Looks like you might not have built Mongoose "
+        "yet.\n\n"
+        "Make sure that... \n\n"
+        "  * You are in your build directory (e.g. Mongoose/_build) and\n"
+        "  * You have built Mongoose ('cmake ..' followed by 'make')\n")
+    sys.exit()
+
+# Check if the supplied matrix download directory exists - if not, create it
+matrix_dir = args.matrix_directory
+if not os.path.exists(matrix_dir):
+    os.makedirs(matrix_dir)
+
+# Make sure the directory ends with '/'
+if not matrix_dir.endswith('/'):
+    matrix_dir = matrix_dir + '/'
+
+# Get the minimum/maximum matrix ID from the arguments
+id_min = args.min
+id_max = args.max
+
+# Download the matrix stats csv file
+testfile = urllib.URLopener()
+testfile.retrieve(
+    "https://www.cise.ufl.edu/research/sparse/matrices/UFstats.csv", 
+    matrix_dir + "/UFstats.csv")
+
+stats_file = matrix_dir + "/UFstats.csv"
+
+# Matrix IDs are not listed in the stats file - we just have to keep count
+matrix_id = 0
+
+with open(stats_file, 'rb') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        if len(row) == 12:
+            matrix_id += 1
+
+            # Check if the matrix ID is in the proper range and 
+            # that the matrix is real and symmetric
+            if (matrix_id >= id_min and 
+                matrix_id <= id_max and 
+                row[2] == row[3] and # Rows = columns (square)
+                row[5] == '1' and    # Real
+                row[10] == '1'):      # Numerically symmetric
+                if args.ids is None or matrix_id in args.ids:
+                    matrix_name = row[0] + '/' + row[1] + '.tar.gz'
+                    gzip_path = matrix_dir + row[0] + '_' + row[1] + '.tar.gz'
+                    matrix_path = matrix_dir + row[1] + '/' + row[1] + ".mtx"
+                    
+                    matrix_exists = os.path.isfile(gzip_path)
+                    if matrix_exists:
+                        tar = tarfile.open(gzip_path, mode='r:gz')
+                        matrix_files = tar.getnames()
+                    else:
+                        # Download matrix if it doesn't exist
+                        print("Downloading " + matrix_name)
+                        testfile.retrieve("https://www.cise.ufl.edu/research/sparse/MM/" + matrix_name, gzip_path)
+                        tar = tarfile.open(gzip_path, mode='r:gz')
+                        matrix_files = tar.getnames()
+                        tar.extractall(path=matrix_dir) # Extract the matrix from the tar.gz file
+                        tar.close()
+
+                    gcv     = find_executable('gcv')
+                    gpmetis = find_executable('gpmetis')
+                    graph_path = matrix_dir + row[1] + '/' + row[1] + ".grf"
+
+                    print("Calling gcv to convert Matrix Market to Graph file...")
+                    call([gcv, "-im", "-oc", matrix_path, graph_path])
+
+                    print("\nCalling Mongoose...")
+                    call(["./bin/mongoose", matrix_path])
+                    
+                    print("\nCalling gpmetis...")
+                    call([gpmetis, graph_path, "2"])
+
+                    # Delete the matrix only if we downloaded it and the keep
+                    # flag is off
+                    if args.purge or not (args.keep or matrix_exists):
+                        files = os.listdir(matrix_dir + row[1])
+                        for file in files:
+                            os.remove(os.path.join(matrix_dir + row[1] + '/', file))
+                        os.rmdir(matrix_dir + row[1])
+                        os.remove(gzip_path)
+
+os.remove(stats_file)
