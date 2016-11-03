53f923940e2a91b910efb2292ffb9fc1ac9639e0
Scott Kolodziej <spkolodziej@gmail.com>
2016-11-03 10:50:53 -0500

    Add Graph/Matrix interop unit tests

--- Tests/Mongoose_UnitTest_Graph_exe.cpp
+++ Tests/Mongoose_UnitTest_Graph_exe.cpp
@@ -1,6 +1,8 @@
 #include "Mongoose_Test.hpp"
 #include "Mongoose_Internal.hpp"
 #include "Mongoose_IO.hpp"
+#include "Mongoose_Interop.hpp"
+#include "Mongoose_CSparse.hpp"
 #include <string>
 #include <stdlib.h>
 
@@ -21,6 +23,17 @@ int main(int argn, char** argv)
     // Test Graph(n, nz) static constructor
     Graph *G2 = Graph::Create(10, 20);
     assert(&G2 != NULL);
+
+    cs *M1 = GraphToCSparse3(G2, false);
+    assert(M1 != NULL);
+    cs *M2 = GraphToCSparse3(G2, true);
+    assert(M2 != NULL);
+
+    SuiteSparse_free(M1);
+    M2->x = NULL;
+    M2->p = NULL;
+    M2->i = NULL;
+    SuiteSparse_free(M2);
 
     SuiteSparse_finish();
 
