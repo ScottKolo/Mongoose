70951ff4eba6c2379174357ca0ae88d2a7ce3c1b
Scott Kolodziej <spkolodziej@gmail.com>
2016-11-10 19:54:44 -0600

    Add C-style string test to I/O test

--- Tests/Mongoose_Test_IO.cpp
+++ Tests/Mongoose_Test_IO.cpp
@@ -26,6 +26,24 @@ void runIOTest(const std::string inputFile, bool validGraph)
     {
         assert(G == NULL);
     }
+
+    // Also try with C-style string
+    Graph *G2 = readGraph(inputFile.c_str());
+
+    if (validGraph)
+    {
+        assert(G2 != NULL);    // A valid graph should not be null
+        assert(G2->n > 0);     // A valid graph should have 
+        assert(G2->nz >= 0);   // At least 1 edge
+        assert(G2->p != NULL); // Column pointers should not be null
+        assert(G2->i != NULL); // Row numbers should not be null
+        G2->~Graph();
+        SuiteSparse_free(G2);
+    }
+    else
+    {
+        assert(G2 == NULL);
+    }
     
     Logger::log(Test, "I/O Test Completed Successfully");
 }
