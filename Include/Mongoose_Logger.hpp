49fde75cd6ef5cc50e7787a24fa8b15ad47d27e2
Scott Kolodziej <spkolodziej@gmail.com>
2017-01-12 13:20:06 -0600

    Make logging C++03 compatible by removing C++11 features

--- Include/Mongoose_Logger.hpp
+++ Include/Mongoose_Logger.hpp
@@ -39,73 +39,14 @@ struct LogData {
 };
 
 // Main Logging Macros
-// Courtesy of http://stackoverflow.com/questions/19415845
 #define LogError(msg) \
-    do { if (LOG_ERROR) (Log(__FILE__, __LINE__, LogData<NoneType>() << msg)); } while (0)
+    do { if (LOG_ERROR) (std::cout << __FILE__ << ":" << __LINE__ << ": " << msg); } while (0)
 #define LogWarn(msg) \
-    do { if (LOG_WARN) (Log(__FILE__, __LINE__, LogData<NoneType>() << msg)); } while (0)
+    do { if (LOG_WARN) (std::cout << __FILE__ << ":" << __LINE__ << ": " << msg); } while (0)
 #define LogInfo(msg) \
-    do { if (LOG_INFO) (Log(LogData<NoneType>() << msg)); } while (0)
+    do { if (LOG_INFO) (std::cout << msg); } while (0)
 #define LogTest(msg) \
-    do { if (LOG_TEST) (Log(LogData<NoneType>() << msg)); } while (0)
-
-// Workaround GCC 4.7.2 not recognizing noinline attribute
-#ifndef NOINLINE_ATTRIBUTE
-#ifdef __ICC
-#define NOINLINE_ATTRIBUTE __attribute__(( noinline ))
-#else
-#define NOINLINE_ATTRIBUTE
-#endif // __ICC
-#endif // NOINLINE_ATTRIBUTE
-
-template<typename List>
-void Log(const char* file, int line,
-         LogData<List>&& data) NOINLINE_ATTRIBUTE
-{
-    std::cout << file << ":" << line << ": ";
-    output(std::cout, std::move(data.list));
-    std::cout << std::endl;
-}
-
-template<typename List>
-void Log(LogData<List>&& data) NOINLINE_ATTRIBUTE
-{
-    output(std::cout, std::move(data.list));
-    std::cout << std::endl;
-}
-
-template<typename Begin, typename Value>
-constexpr LogData<std::pair<Begin&&, Value&&>> operator<<(LogData<Begin>&& begin,
-                                                          Value&& value) noexcept
-{
-    return {{ std::forward<Begin>(begin.list), std::forward<Value>(value) }};
-}
-
-template<typename Begin, size_t n>
-constexpr LogData<std::pair<Begin&&, const char*>> operator<<(LogData<Begin>&& begin,
-                                                              const char (&value)[n]) noexcept
-{
-    return {{ std::forward<Begin>(begin.list), value }};
-}
-
-typedef std::ostream& (*PfnManipulator)(std::ostream&);
-
-template<typename Begin>
-constexpr LogData<std::pair<Begin&&, PfnManipulator>> operator<<(LogData<Begin>&& begin,
-                                                                 PfnManipulator value) noexcept
-{
-    return {{ std::forward<Begin>(begin.list), value }};
-}
-
-template <typename Begin, typename Last>
-void output(std::ostream& os, std::pair<Begin, Last>&& data)
-{
-    output(os, std::move(data.first));
-    os << data.second;
-}
-
-inline void output(std::ostream& os, NoneType nType)
-{ }
+    do { if (LOG_TEST) (std::cout << msg); } while (0)
 
 namespace Mongoose
 {
