9f5b723647c9e7cd025f1337f7b9b3c46f1088d7
Scott Kolodziej <spkolodziej@gmail.com>
2017-06-15 09:29:56 -0500

    Fix segfault - wrong graph referenced

--- Source/Mongoose_Refinement.cpp
+++ Source/Mongoose_Refinement.cpp
@@ -68,11 +68,11 @@ Graph *refine(Graph *graph, Options *options)
             /* Load up the inverse matching */
             Int v[3] = {-1, -1, -1};
             v[0] = invmatchmap[k];
-            v[1] = graph->getMatch(v[0]);
+            v[1] = P->getMatch(v[0]);
             if (v[0] == v[1]) { v[1] = -1; }
             else
             {
-                v[2] = graph->getMatch(v[1]);
+                v[2] = P->getMatch(v[1]);
                 if (v[0] == v[2]) { v[2] = -1; }
             }
 
