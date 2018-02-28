010fdaaa074bd4dc2dfed91997d3c58116122ef1
Scott Kolodziej <spkolodziej@gmail.com>
2018-02-28 09:02:25 -0600

    Remove unimplemented label propagation prototype

--- Include/Mongoose_Matching.hpp
+++ Include/Mongoose_Matching.hpp
@@ -24,7 +24,6 @@ void matching_Random(Graph *, const Options *);
 void matching_HEM(Graph *, const Options *);
 void matching_PA(Graph *, const Options *);
 void matching_DavisPA(Graph *, const Options *);
-void matching_LabelProp(Graph *, const Options *);
 void matching_Cleanup(Graph *, const Options *);
 
 } // end namespace Mongoose
