1a82639a8721901a30fe2d4ad6fb2bac77c90e80
Scott Kolodziej <spkolodziej@gmail.com>
2018-05-25 13:08:56 -0500

    Fix MATLAB coarsen documentation, remove unused test file

--- MATLAB/coarsen.m
+++ MATLAB/coarsen.m
@@ -1,6 +1,6 @@
 function [G_coarse, A_coarse, map] = coarsen (G, (O, A))
 %COARSEN coarsen a graph unsafely but quickly.
-%   mongoose_coarsen(G) computes a matching of vertices in the graph G
+%   coarsen(G) computes a matching of vertices in the graph G
 %   and then coarsens the graph by combining all matched vertices into
 %   supervertices. It assumes that the matrix G provided has an all zero
 %   diagonal, is symmetric, and has all positive edge weights. With no 
@@ -11,12 +11,24 @@ function [G_coarse, A_coarse, map] = coarsen (G, (O, A))
 %   matrix is of the correct form, and may provide erroneous results if 
 %   used incorrectly.
 %
+%   [G_coarse, A_coarse, map] = coarsen(G) coarsens a graph represented with 
+%   sparse adjacency matrix G. G_coarse is the coarsened adjacency matrix, 
+%   A_coarse is the coarsened array of vertex weights, and map is a mapping of
+%   original vertices to coarsened vertices.
+%
+%   [G_coarse, A_coarse, map] = coarsen(G, O) uses the option struct O to
+%   specify coarsening options (e.g. matching strategies).
+%
+%   [G_coarse, A_coarse, map] = coarsen(G, O, A) uses the array A as vertex
+%   weights, such that A(i) is the vertex weight of vertex i. If A is not
+%   specified, A is assumed to be an array of all ones (all weights are one).
+%
 %   Example:
-%       Prob = UFget('DNVS/troll'); A = Prob.A;
-%       G = mongoose_sanitizeMatrix(A);
+%       Prob = ssget('DNVS/troll'); A = Prob.A;
+%       G = sanitize(A);
 %       G_coarse = coarsen(G);
 %       spy(G_coarse);
 %
-%   See also COARSEN, DEFAULTOPTIONS.
+%   See also SAFE_COARSEN, DEFAULTOPTIONS.
 
 error ('coarsen mexFunction not found') ;
